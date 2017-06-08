38eae94863fc77d912cc6a1e3dd124ae4ebdf5b5
Yoo Jaehong <hibria21@gmail.com>
2017-06-09 03:18:33 +0900

    until training

---
+++ backprop.py
@@ -0,0 +1,171 @@
+from math import exp
+from random import seed
+from random import random
+import numpy as np
+import os
+import cv2
+from struct import unpack
+from scipy import misc
+from urllib import request
+import gzip
+
+# MNIST DataSet URLs
+url_train_image = 'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz'
+url_train_label = 'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz'
+url_test_image = 'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz'
+url_test_label = 'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz'
+
+urls = {
+        'train': {
+            'image': url_train_image,
+            'label': url_train_label
+            },
+        'test': {
+            'image': url_test_image,
+            'label': url_test_label
+            }
+        }
+
+# Initialize a network
+def initialize_network(n_inputs, n_hidden, n_outputs):
+    network = list()
+    hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+    network.append(hidden_layer)
+    output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+    network.append(output_layer)
+    return network
+
+# Calculate neuron activation for an input
+def activate(weights, inputs):
+    activation = weights[-1]
+    for i in range(len(weights)-1):
+        activation += weights[i] * inputs[i]
+    return activation
+
+# Transfer neuron activation
+def transfer(activation):
+    return 1.0 / (1.0 + exp(-activation))
+
+# Forward propagate input to a network output
+def forward_propagate(network, row):
+    inputs = row
+    for layer in network:
+        new_inputs = []
+        for neuron in layer:
+            activation = activate(neuron['weights'], inputs)
+            neuron['output'] = transfer(activation)
+            new_inputs.append(neuron['output'])
+        inputs = new_inputs
+    return inputs
+
+# Calculate the derivative of an neuron output
+def transfer_derivative(output):
+    return output * (1.0 - output)
+
+# Backpropagate error and store in neurons
+def backward_propagate_error(network, expected):
+    for i in reversed(range(len(network))):
+        layer = network[i]
+        errors = list()
+        if i != len(network)-1:
+            for j in range(len(layer)):
+                error = 0.0
+                for neuron in network[i + 1]:
+                    error += (neuron['weights'][j] * neuron['delta'])
+                errors.append(error)
+        else:
+            for j in range(len(layer)):
+                neuron = layer[j]
+                errors.append(expected[j] - neuron['output'])
+        for j in range(len(layer)):
+            neuron = layer[j]
+            neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
+
+# Update network weights with error
+def update_weights(network, row, l_rate):
+    for i in range(len(network)):
+        inputs = row[:-1]
+        if i != 0:
+            inputs = [neuron['output'] for neuron in network[i - 1]]
+        for neuron in network[i]:
+            for j in range(len(inputs)):
+                neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
+            neuron['weights'][-1] += l_rate * neuron['delta']
+
+# Train a network for a fixed number of epochs
+def train_network(network, train, l_rate, n_epoch, n_outputs):
+    for epoch in range(n_epoch):
+        sum_error = 0
+        for row in train:
+            outputs = forward_propagate(network, row)
+            expected = [0 for i in range(n_outputs)]
+            expected[row[-1]] = 1
+            sum_error += sum([(expected[i]-outputs[i])**2 for i in range(len(expected))])
+            backward_propagate_error(network, expected)
+            update_weights(network, row, l_rate)
+        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+
+
+def make_dataset(stage):
+    dataset = []
+    fp_image = gzip.open(request.urlretrieve(urls[stage]['image'])[0], 'rb')
+    fp_label = gzip.open(request.urlretrieve(urls[stage]['label'])[0], 'rb')
+
+    img = np.zeros((28, 28))  # 이미지가 저장될 부분
+
+    index = 0
+
+    # drop header info?
+    s = fp_image.read(16)    # read first 16byte
+    l = fp_label.read(8)     # read first  8byte
+
+    # 숫자 데이터를 읽어서 해당하는 데이터를 지정하고 출력
+    k = 0                    # 테스트용 index
+    # read mnist and show number
+    while True:
+        # 784바이트씩 읽음
+        s = fp_image.read(784)
+        # 1바이트씩 읽음
+        l = fp_label.read(1)
+
+        if not s:
+            break
+        if not l:
+            break
+        label = int(l[0])
+        # unpack
+        img = np.reshape(unpack(len(s) * 'B', s), (28, 28))
+        # resize from 28x28 to 10x10
+        resized = misc.imresize(img, (10, 10))
+        # binarization
+        ret, thresh = cv2.threshold(resized, 75, 255, cv2.THRESH_BINARY)
+        # save binary image
+        datum = list(resized.flat)
+        datum.append(label)
+        
+        dataset.append(datum)
+    print(len(dataset))
+    return dataset
+    
+
+    print (datum)
+
+def normalize_dataset(dataset):
+    for row in dataset:
+        for i in range(len(row)-1):
+            row[i] = row[i] / 255
+
+if __name__ == "__main__":
+    # Test training backprop algorithm
+    dataset = make_dataset('test')
+    normalize_dataset(dataset)
+    n_inputs = len(dataset[0]) - 1
+    n_outputs = len(set([row[-1] for row in dataset]))
+
+    network = initialize_network(n_inputs, 11, n_outputs)
+    train_network(network, dataset, 0.5, 20, n_outputs)
+    for layer in network:
+        print(layer)
+
+
+
